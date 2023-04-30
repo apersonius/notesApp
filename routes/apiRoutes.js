cc8e6b8a1090c8e673228883f039850a973c37c5
apersonius <personius12@gmail.com>
2023-04-30 00:07:32 -0500

    add api code to create and send notes created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,27 @@
-// api route
+const router = require('express').Router();
+const fs = require('fs');
+
+var uid = require('uid');
+
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db/json'));
+    })
+};
+
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.JSON(db)
+
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+
+            id: uid(),
+        };
+
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.JSON(db);
+    });
