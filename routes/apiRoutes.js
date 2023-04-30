51132103a5784cf47dee7d563d02429cd87ff745
apersonius <personius12@gmail.com>
2023-04-30 00:37:09 -0500

    fix errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,18 @@
-const router = require('express').Router();
+const app = require('express').Router();
 const fs = require('fs');
 
 var uniqid = require('uniqid');
 
 module.exports = (app) => {
-    app.get('/api/notes', (req, res) => {
+    app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../db/db/json'));
     })
 };
 
-    app.post('/api/notes', (req, res) => {
+    app.post('/notes', (req, res) => {
         let db = fs.readFileSync('db/db.json');
-        db = JSON.parse(db);
-        res.JSON(db)
+        db = json.parse(db);
+        res.json(db)
 
         let userNote = {
             title: req.body.title,
